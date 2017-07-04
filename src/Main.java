fcc1e6fed3eb53a7ff0dfbe54f1a17dfedb86e01
Nekonoha <tan.fantazma@gmail.com>
2017-07-04 14:57:39 +0900

    project started

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import processing.core.PApplet;
+
+public class Main extends PApplet{
+    public void settings() {
+        size(400, 600);
+    }
+
+    public void setup() {
+        background(0);
+    }
+
+    public void draw() {
+        background(0);
+    }
+
+    public static void main(String args[]) {
+        PApplet.main("Main");
+    }
+}
