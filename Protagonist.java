ccba1825d5de7503332afa2484efa294fed95173
RkKnn <sankaku9006221@gmail.com>
2017-07-04 20:07:01 +0900

    a

---
+++ Protagonist.java
@@ -0,0 +1,11 @@
+public class Protagonist extends PRect{
+
+    public Protagonist(int x, int y, int width, int height) {
+        super(x, y, width, height);
+    }
+
+
+    public void move(){
+
+    }
+}
