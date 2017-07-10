08418cd2bba55aae9d00ebf83559005f9454055f
ctare <sankaku9006221@yahoo.co.jp>
2017-07-10 14:03:24 +0900

    create jump

--- Protagonist.java
+++ Protagonist.java
@@ -1,4 +1,5 @@
 public class Protagonist extends PRect{
+    private float ySpeed, yAcceleration = 0.2f;
 
     public Protagonist(float x, float y, int width, int height) {
         super(x, y, width, height);
@@ -10,6 +11,13 @@ public class Protagonist extends PRect{
     }
 
     public void jump(){
+        ySpeed = -5;
+    }
 
+    @Override
+    public void update() {
+        Main.p.rect(super.x, super.y, super.width, super.height);
+        y += ySpeed;
+        ySpeed += yAcceleration;
     }
 }
